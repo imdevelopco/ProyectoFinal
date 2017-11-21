b4554848c39f56ee661c14fd7f9c0eb6961dfc57
david <davidsanciaz@gmail.com>
2017-11-21 14:02:38 -0500

    Atributos Clase Persona, Faltan algunos

--- ProyectoFinal/include/Persona.h
+++ ProyectoFinal/include/Persona.h
@@ -1,6 +1,9 @@
 #ifndef PERSONA_H
 #define PERSONA_H
+#include <iostream>
+#include "Telefono.h"
 
+using namespace std;
 
 class Persona
 {
@@ -9,6 +12,17 @@ class Persona
         virtual ~Persona();
 
     protected:
+      //Atributos de las Personas
+      string 
+
+      /*nombres completos,
+       apellidos completos,
+       número telefónico por defecto,
+        dirección de residencia,fecha de nacimiento
+        (recuerde que la Fecha de nacimiento se compone de día, mes y año en su forma más básica),
+         tipo de documento de
+          identificación (CC, CE, PT, TI, RC), y
+          número de documento.*/
 
     private:
 };
