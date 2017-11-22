1879b60e7008727867a30bfad4e17e43844dba6f
CamiloArias47 <camillo47@gmail.com>
2017-11-22 06:31:01 -0500

    Creación geters y seters clase Silla

--- ProyectoFinal/include/Silla.h
+++ ProyectoFinal/include/Silla.h
@@ -1,16 +1,32 @@
 #ifndef SILLA_H
 #define SILLA_H
+#include <iostream>
 
+using namespace std;
 
 class Silla
 {
     public:
         Silla();
+        Silla(string numero, string tipo);
         virtual ~Silla();
 
+        /*---------------------------------------*/
+        /*                SETERS                 */
+        /*---------------------------------------*/
+        void setNumber(string num);
+        void setType(string tipo);
+
+        /*---------------------------------------*/
+        /*                GETERS                 */
+        /*---------------------------------------*/
+        string getNumber();
+        string getType();
     protected:
 
     private:
+      string numero;
+      string tipo; // opciones ["preferencial", "normal", "bajoCosto"]
 };
 
 #endif // SILLA_H
