13e3082b3e1044bfa5ac4f480b0e46f76b1c53eb
CamiloArias47 <camillo47@gmail.com>
2017-11-23 23:26:05 -0500

    Clase aerolinea

--- ProyectoFinal/include/Aerolinea.h
+++ ProyectoFinal/include/Aerolinea.h
@@ -1,16 +1,30 @@
 #ifndef AEROLINEA_H
 #define AEROLINEA_H
+#include <iostream>
+#include <vector>
+#include "CompaniaAerea.h"
+#include "Representante.h"
+#include "Tiquete.h"
+#include "Telefono.h"
+#include "Cliente.h"
+#include "Aeropuerto.h"
 
+using namespace std;
 
-class Aerolinea
+class Aerolinea : public CompaniaAerea
 {
     public:
         Aerolinea();
+        Aerolinea(string nit, string razonSocial, string direccion, string paginaWeb,  vector<Telefono> agenda, Representante representante, vector<Tiquete> transaciones, vector<Cliente> clientesGolden, string matriculaAerea, int totalAvionesFlota, vector<Aeropuerto> aeropuertos);
         virtual ~Aerolinea();
 
     protected:
 
     private:
+      string matriculaAerea;
+      int totalAvionesFlota;
+      vector<Aeropuerto> aeropuertos;
+
 };
 
 #endif // AEROLINEA_H
--- ProyectoFinal/src/Aerolinea.cpp
+++ ProyectoFinal/src/Aerolinea.cpp
@@ -5,6 +5,10 @@ Aerolinea::Aerolinea()
     //ctor
 }
 
+Aerolinea::Aerolinea(string nit, string razonSocial, string direccion, string paginaWeb,  vector<Telefono> agenda, Representante representante, vector<Tiquete> transaciones, vector<Cliente> clientesGolden, string matriculaAerea, int totalAvionesFlota, vector<Aeropuerto> aeropuertos):CompaniaAerea(nit, razonSocial, direccion, paginaWeb, agenda, representante, transaciones, clientesGolden){
+
+}
+
 Aerolinea::~Aerolinea()
 {
     //dtor
