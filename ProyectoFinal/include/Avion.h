b49b5a39e57f6c2f2563523cc124b7aff0a7957c
CamiloArias47 <camillo47@gmail.com>
2017-11-28 00:06:04 -0500

    Geters y seters clase Avion

--- ProyectoFinal/include/Avion.h
+++ ProyectoFinal/include/Avion.h
@@ -1,16 +1,66 @@
 #ifndef AVION_H
 #define AVION_H
+#include <iostream>
+#include <vector>
+#include "Silla.h"
+#include "Aeropuerto.h"
 
+using namespace std;
 
 class Avion
 {
     public:
         Avion();
+        Avion(vector<Silla> preferencial, vector<Silla> normal, vector<Silla> bajoCosto, string matricula, Aeropuerto origen, Aeropuerto destino, int cantidadGasolina, int tiempoMaximo);
         virtual ~Avion();
+
+        /*---------------------------------------*/
+        /*                SETERS                 */
+        /*---------------------------------------*/
+        void setSillasPreferencial(vector<Silla> sillas);
+        void setSillasNormal(vector<Silla> sillas);
+        void setSillasBajoCosto(vector<Silla> sillas);
+        void setMatricula(string matricula);
+        void setStatus(string status);
+        void setTotalDisponiblePreferencial(int tot);
+        void setTotalDisponibleNormal(int tot);
+        void setTotalDisponibleBajo(int tot);
+        void setAeropuertoOrigen(Aeropuerto origen);
+        void setAeropuertoDestino(Aeropuerto destino);
+        void setCantidadGasolina(int cant);
+        void setTiempoMaximo(int tiempo);
+
+        /*---------------------------------------*/
+        /*                GETERS                 */
+        /*---------------------------------------*/
+        vector<Silla> getSillasPreferencial();
+        vector<Silla> getSillasNormal();
+        vector<Silla> getSillasBajoCosto();
+        string getMatricula();
+        string getStatus();
+        int getTotalDisponiblePreferencial();
+        int getTotalDisponibleNormal();
+        int getTotalDisponibleBajo();
+        Aeropuerto getAeropuertoOrigen();
+        Aeropuerto getAeropuertoDestino();
+        int getCantidadGasolina();
+        int getTiempoMaximo();
 
     protected:
 
     private:
+      vector<Silla> preferencial;
+      vector<Silla> normal;
+      vector<Silla> bajoCosto;
+      string matricula;
+      string estado;
+      int disponiblePreferencial;
+      int disponibleNormal;
+      int disponibleBajoCosto;
+      Aeropuerto origen;
+      Aeropuerto destino;
+      int cantidadGasolina;
+      int tiempoMaximo;
 };
 
 #endif // AVION_H
--- ProyectoFinal/src/Avion.cpp
+++ ProyectoFinal/src/Avion.cpp
@@ -2,10 +2,216 @@
 
 Avion::Avion()
 {
-    //ctor
+  //Crear las sillas preferenciales
+  for (int i = 0; i < 10; i++) {
+    Silla silla("p-"+i,"preferencial");
+    this->preferencial.push_back(silla);
+  }
+
+  //Crear las sillas normales
+  for(int i = 0; i < 10; ++i){
+    Silla silla("n-"+i,"normal");
+    this->normal.push_back(silla);
+  }
+
+  //crear las sillas de bajo bajo costo
+  for(int i = 0; i < 10; i++){
+    Silla silla("b-"+i,"bajo costo");
+    this->bajoCosto.push_back(silla);
+  }
+
+  this->matricula               = "matricula";
+  this->estado                  = "Excelente";
+  this->disponiblePreferencial  = this->preferencial.size();
+  this->disponibleNormal        = this->normal.size();
+  this->disponibleBajoCosto     = this->bajoCosto.size();
+  this->cantidadGasolina        = 1000;
+  this->tiempoMaximo            = 50;
+
+  /*Se debe establecer el aeropuerto de origen y de destino por medio de los setters*/
+}
+
+Avion::Avion(vector<Silla> preferencial, vector<Silla> normal, vector<Silla> bajoCosto, string matricula, Aeropuerto origen, Aeropuerto destino, int cantidadGasolina, int tiempoMaximo){
+  this->preferencial            = preferencial;
+  this->normal                  = normal;
+  this->bajoCosto               = bajoCosto;
+  this->matricula               = matricula;
+  this->estado                  = "Excelente";
+  this->disponiblePreferencial  = preferencial.size();
+  this->disponibleNormal        = normal.size();
+  this->disponibleBajoCosto     = bajoCosto.size();
+  this->origen                  = origen;
+  this->destino                 = destino;
+  this->cantidadGasolina        = cantidadGasolina;
+  this->tiempoMaximo            = tiempoMaximo;
 }
 
 Avion::~Avion()
 {
     //dtor
+}
+
+/* +───────────────────────────────────────────────────────────────────────+ */
+/*       /$$$$$$  /$$$$$$$$ /$$$$$$$$ /$$$$$$$$ /$$$$$$$   /$$$$$$         */
+/*		 /$$__  $$| $$_____/|__  $$__/| $$_____/| $$__  $$ /$$__  $$         */
+/*		| $$  \__/| $$         | $$   | $$      | $$  \ $$| $$  \__/         */
+/*		|  $$$$$$ | $$$$$      | $$   | $$$$$   | $$$$$$$/|  $$$$$$          */
+/*		 \____  $$| $$__/      | $$   | $$__/   | $$__  $$ \____  $$         */
+/*		 /$$  \ $$| $$         | $$   | $$      | $$  \ $$ /$$  \ $$         */
+/*		|  $$$$$$/| $$$$$$$$   | $$   | $$$$$$$$| $$  | $$|  $$$$$$/         */
+/*		 \______/ |________/   |__/   |________/|__/  |__/ \______/          */
+/* +───────────────────────────────────────────────────────────────────────+ */
+/*Establece las sillas preferenciales del avion
+  setSillasPreferencial(sillas) -> void
+  sillas                        == vector<Silla>
+*/
+void Avion::setSillasPreferencial(vector<Silla> sillas){
+  this->preferencial = sillas;
+}
+
+/*Establece las sillas normales del avion
+  setSillasNormal(sillas) -> void
+  sillas                 == vector<Silla>
+*/
+void Avion::setSillasNormal(vector<Silla> sillas){
+  this->normal = sillas;
+}
+
+/*Establece las sillas de bajo costo dl avion
+  setSillasBajoCosto(sillas) -> void
+  sillas                     == vector<Silla>
+*/
+void Avion::setSillasBajoCosto(vector<Silla> sillas){
+  this->bajoCosto = sillas;
+}
+
+/*Establece la matricula del avion
+  setMatricula(matricula) -> void
+  matricula               == string
+*/
+void Avion::setMatricula(string matricula){
+  this->matricula = matricula;
+}
+
+/*Establece el status del avion
+  setStatus(status) -> void
+  status            == string
+*/
+void Avion::setStatus(string status){
+  this->estado = status;
+}
+
+/*Establece el total de sillas preferenciales disponibles
+  setTotalDisponiblePreferencial(tot) -> void
+  tot                                 == int
+*/
+void Avion::setTotalDisponiblePreferencial(int tot){
+  this->disponiblePreferencial = tot;
+}
+
+/*Establece el total de sillas normales disponibles
+  setTotalDisponibleNormal(tot) -> void
+  tot                           == int      Total de sillas normales disponibles
+*/
+void Avion::setTotalDisponibleNormal(int tot){
+  this->disponibleNormal = tot;
+}
+
+/*Establece el total de sillas  a bajo costo disponibles
+  setTotalDisponibleBajo(tot) -> void
+  tot                         == int      Total de sillas bajo costo disponibles
+*/
+void Avion::setTotalDisponibleBajo(int tot){
+  this->disponibleBajoCosto = tot;
+}
+
+/*Establece el aeropuesto de origen del avion
+  setAeropuertoOrigen(origen) -> void
+  origen                      == Aeropuerto
+*/
+void Avion::setAeropuertoOrigen(Aeropuerto origen){
+  this->origen = origen;
+}
+
+/*Establece el aeropuerto de destino de el avión
+  setAeropuertoDestino(destino) -> void
+  destino                       == Aeropuerto
+*/
+void Avion::setAeropuertoDestino(Aeropuerto destino){
+  this->destino = destino;
+}
+
+/*Establece la cantidad de gasolina que le cabe al avion
+  setCantidadGasolina(cant) -> void
+  cant                      == int
+*/
+void Avion::setCantidadGasolina(int cant){
+  this->cantidadGasolina = cant;
+}
+
+/*Establece el tiempo maximo de vuelo del avion
+  setTiempoMaximo(tiempo) -> void;
+  tiempo                  == int      Tiempo en horas de vuelo
+*/
+void Avion::setTiempoMaximo(int tiempo){
+  this->tiempoMaximo = tiempo;
+}
+
+
+/* +───────────────────────────────────────────────────────────────────────+ */
+/*	      /$$$$$$  /$$$$$$$$ /$$$$$$$$ /$$$$$$$$ /$$$$$$$   /$$$$$$ 	       */
+/*       /$$__  $$| $$_____/|__  $$__/| $$_____/| $$__  $$ /$$__  $$		     */
+/*      | $$  \__/| $$         | $$   | $$      | $$  \ $$| $$  \__/		     */
+/*      | $$ /$$$$| $$$$$      | $$   | $$$$$   | $$$$$$$/|  $$$$$$  		     */
+/*      | $$|_  $$| $$__/      | $$   | $$__/   | $$__  $$ \____  $$		     */
+/*      | $$  \ $$| $$         | $$   | $$      | $$  \ $$ /$$  \ $$		     */
+/*      |  $$$$$$/| $$$$$$$$   | $$   | $$$$$$$$| $$  | $$|  $$$$$$/         */
+/*       \______/ |________/   |__/   |________/|__/  |__/ \______/          */
+/* +───────────────────────────────────────────────────────────────────────+ */
+vector<Silla> Avion::getSillasPreferencial(){ //retorna el array de sillas  pre-
+    return this->preferencial;                //ferenciales
+}
+
+vector<Silla> Avion::getSillasNormal(){//retorna el array de dillas normales que
+  return this->normal;                 //tiene el avion
+}
+
+vector<Silla> Avion::getSillasBajoCosto(){//retorna  el array  de sillas de bajo
+  return this->bajoCosto;                 //costo que tiene el avion
+}
+
+string Avion::getMatricula(){//retorna la matricula del avión
+  return this->matricula;
+}
+
+string Avion::getStatus(){//retorna el estado de avion
+  return this->estado;
+}
+
+int Avion::getTotalDisponiblePreferencial(){//retorna la  cantidad disponible de
+  return this->disponiblePreferencial;      //sillas preferenciales en el avion
+}
+
+int Avion::getTotalDisponibleNormal(){//retorna la cantidad  de sillas  normales
+  return this->disponibleNormal;      //disponibles en el avión
+}
+
+int Avion::getTotalDisponibleBajo(){//retorna  el total  de sillas de bajo costo
+  return this->disponibleBajoCosto; //disponibles en el avion
+}
+
+Aeropuerto Avion::getAeropuertoOrigen(){//retorna  el  aeropuerto  de origen del
+  return this->origen;                  //avión
+}
+
+Aeropuerto Avion::getAeropuertoDestino(){//retorna  el  aeropuesro de destino de
+  return this->destino;                  //el avion
+}
+
+int Avion::getCantidadGasolina(){//retorna  la  capacidad  total de gasolina del
+  return this->cantidadGasolina; //avion;
+}
+
+int Avion::getTiempoMaximo(){//retorna el tiempo maximo de vuelo de el aavion
+  return this->tiempoMaximo;
 }
